71969711b3d3319496cab27e1d83de732a0b5b0d
xsz758521 <1771368146@qq.com>
2019-01-06 15:18:51 +0800

    left又提交了

--- src/main/java/com/itheima/controller/leftController.java
+++ src/main/java/com/itheima/controller/leftController.java
@@ -3,5 +3,6 @@ package com.itheima.controller;
 public class leftController {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("啊啊啊");
     }
 }
